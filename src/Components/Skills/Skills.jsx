f34a975db298adc37e40f6c28c512629fbfde190
A1-mamun <1154almamun@gmail.com>
2024-10-20 09:03:56 +0600

    Responsive bug solve

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -13,7 +13,7 @@ const Skills = () => {
       <h2 className="text-center text-2xl md:text-3xl lg:text-4xl font-bold text-white">
         Tools and Technologies I Use
       </h2>
-      <div className="flex flex-wrap justify-center  gap-5 md:gap-7 lg:gap-10 mt-16 md:mt-24 lg:mt-20 animate-pulse">
+      <div className="flex flex-wrap justify-center  gap-5 md:gap-7 lg:gap-10 my-12 md:my-16 lg:my-20 animate-pulse">
         <img
           className="w-12 h-12 md:w-16 md:h-16 lg:w-20 lg:h-20 hover:scale-125 duration-300"
           src={c}
@@ -59,6 +59,26 @@ const Skills = () => {
           src={firebase}
           alt="Firebase"
         />
+      </div>
+      <h2 className="text-center text-2xl md:text-3xl lg:text-4xl font-bold text-white">
+        Soft Skills
+      </h2>
+      <div className="flex gap-3 my-12 md:my-16 lg:my-20">
+        <h3 className="text-base md:text-lg lg:text-xl font-semibold text-slate-400">
+          Campus Ambassador:
+        </h3>
+        <div className="lg:flex-1 space-y-1 font-medium">
+          <p className="text-base md:text-lg lg:text-xl font-medium text-white">
+            Works as a Campus Ambassador of <strong>Programming Hero</strong>
+          </p>
+          <p className="text-sm md:text-base lg:text-lg text-slate-400">
+            Work to raise student awareness through the title{" "}
+            <strong>'Let's Code Your Career through Programming'</strong>
+          </p>
+          <p className="text-sm md:text-base lg:text-lg font-semibold text-white">
+            <stromg> February 2024 - Present</stromg>
+          </p>
+        </div>
       </div>
     </section>
   );
